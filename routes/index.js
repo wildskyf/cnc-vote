bed63694b12e617c66517f90d5286a41dd9f3ede
wildskyf <poppin.wildsky@gmail.com>
2017-12-30 15:01:58 +0000

    finish basic candidate render

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,55 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.post('/vote_info_check', (req, res, next) => {
-  const token = req.body;
+router.post('/', (req, res, next) => {
+  const { token } = req.body;
 
-  res.render('vote_info_check', {
-    session: '第一場',
-    group: 'A組',
-    name: '無威鄰大大'
-  });
+  // FIXME: avoid SQL injection
+  connection.query(`select roundId from voters where token = '${token}'`, (err, result) => {
+    const { roundId } = result[0];
+
+    if (result.length == 0) {
+      res.render('index', {
+        token: token,
+        error: {
+          message: "Wrong token format."
+        }
+      });
+    }
+    else {
+      res.render('post_redirect', {
+        r_id: rountId,
+        token: token
+      })
+    }
+
+  })
+
+});
+
+router.post('/vote_info_check', (req, res, next) => {
+  const { token, r_id } = req.body;
+
+  connection.query(``, (err, result) => {
+    res.render('vote_info_check', {
+      token: token,
+      r_id: r_id,
+      session: '第一場',
+      group: 'A組',
+      name: '無威鄰大大'
+    });
+  })
 });
 
 router.get('/show_candidate', (req, res, next) => {
-  res.render('show_candidate', {
+  const { token, r_id } = req.body;
 
+  connection.query(`select candidateId, chName from candidatesForVote natural join candidates where roundId = 1 ;`, (err, result) => {
+    res.render('show_candidate', {
+      r_id: r_id,
+      token: token,
+      candidates: result
+    });
   });
 });
 
